f7a7676d9067e73936d1b0e8558522a37cf4274b
Danielle Thompson <danithompson74@gmail.com>
2021-02-01 13:36:29 -0800

    Add tests folder & new file
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

---
+++ __tests__/whiteboard_scripts.test.js
@@ -0,0 +1,8 @@
+//import statement
+
+// describe('EarthlingData', () => {
+
+//     test('should correctly return EarthlingData age', () => {
+//         const earthling = new EarthlingData(12);
+//         expect(earthling.earthAge).toEqual(12);
+//     });
